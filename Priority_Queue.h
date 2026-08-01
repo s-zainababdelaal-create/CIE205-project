e3d733a26bc5924497fe0547c78609d4c978d702
s-ahmedgaber-ctrl <s-ahmed.gaber@zewailcity.edu.eg>
2026-08-01 20:44:17 +0300

    .

--- Priority_Queue.h
+++ Priority_Queue.h
@@ -1,14 +1,16 @@
 #pragma once
+#include <iostream>
+using namespace std;
 template <typename T>
 class PQNode {
 private:
     T* data;             // pointer to data Patient
     int priority;   // priority = CT + α × TESTS
-    PQNode<T>* next;
+    PQNode<T>* next = nullptr;
 public:
     T* getData();
     PQNode<T>* getNext();
-    double getPriority();
+    int getPriority();
     void setNext(PQNode<T>* next);
     void setData(T* item);
     void setPriority(double p);
@@ -23,12 +25,13 @@ class PriorityQueue {
 public:
     PriorityQueue();
     ~PriorityQueue();
-    void enqueue(T* item,int priority);
-    T* dequeue();                           
-    T* removeByID(int id);
-    bool ismpty();
+    void enqueue(T* item, int priority);
+    T* dequeue();
+    T* removeByID(int id);   // "No structural duplication or shallow copies (Patient p2 = p1) are allowed." (SHARE, MOVE, BUT DON'T COPY)
+    bool isEmpty();
     int getSize();
     void display();
 };
 
-//Test
+//#include "Priority_Queue.cpp"
+
