374dd195ffda721783d8be848901d6b9cc863556
s-ahmedgaber-ctrl <s-ahmed.gaber@zewailcity.edu.eg>
2026-08-03 21:35:32 +0300

    Update Priority_Queue.h

--- Priority_Queue.h
+++ Priority_Queue.h
@@ -1,37 +1,42 @@
-#pragma once
-#include <iostream>
-using namespace std;
 template <typename T>
-class PQNode {
+class PQNode  
+    // PQNode<T> — Node class for Priority Queue
+  {
 private:
-    T* data;             // pointer to data Patient
-    int priority;   // priority = CT + α × TESTS
-    PQNode<T>* next = nullptr;
+    T* data;             // Pointer to stored object to avoid copying memory
+    int priority;        // Computed priority (CT + alpha * TESTS)
+	PQNode<T>* next = nullptr; // Pointer to next node in list and nullptr if last node by default
+
 public:
+    // Getters
     T* getData();
     PQNode<T>* getNext();
     int getPriority();
+
+    // Setters
     void setNext(PQNode<T>* next);
     void setData(T* item);
-    void setPriority(double p);
+    void setPriority(double p); // Note: double converted to int
 };
 
-template <typename T>
 
-class PriorityQueue {
-    int count = 0;
-    PQNode<T>* Front = nullptr;
+template <typename T>
+class PriorityQueue  // PriorityQueue<T> — Hand-built Sorted Linked List Queue
+{
+    int count = 0;              // Current queue size
+    PQNode<T>* Front = nullptr; // Pointer to highest priority node (Head)
 
 public:
     PriorityQueue();
-    ~PriorityQueue();
-    void enqueue(T* item, int priority);
-    T* dequeue();
-    T* removeByID(int id);   // "No structural duplication or shallow copies (Patient p2 = p1) are allowed." (SHARE, MOVE, BUT DON'T COPY)
-    bool isEmpty();
-    int getSize();
-    void display();
+    ~PriorityQueue();           // Deletes PQNodes only, keeps actual data
+
+    void enqueue(T* item, int priority); // Inserts item in sorted order
+    T* dequeue();              // Removes & returns highest priority item
+    T* removeByID(int id);      // Removes specific patient by ID (for Leave event)
+
+    bool isEmpty(); // Make sure to check if queue is empty before dequeueing or removing by ID
+	int getSize();  // Returns current queue size
+    void display();            //for  print
 };
 
-//#include "Priority_Queue.cpp"
-
+//#include "Priority_Queue.cpp" // Included for template compilation (checkpoint 2)
