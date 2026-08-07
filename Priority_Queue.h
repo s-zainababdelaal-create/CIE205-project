aa010fb48d827618711ef81e6f6df8429871934b
Haneen-Omar <s-haneen.omar@zewailcity.edu.eg>
2026-08-07 09:28:33 +0300

    Update Priority_Queue.h

--- Priority_Queue.h
+++ Priority_Queue.h
@@ -46,3 +46,15 @@ public:
 };
 
 //#include "Priority_Queue.cpp" // Included for template compilation (checkpoint 2)
+
+template <typename T>
+void PriorityQueue<T>::collectExpiredIDs(int currentTime, int limit, LinkedList<int>& outIDs) const {
+    PQNode<T>* current = Front;
+    while (current != nullptr) {
+        int waitTime = currentTime - current->getData()->getCheckInTime();
+        if (waitTime > limit) {
+            outIDs.insertEnd(current->getData()->getID());
+        }
+        current = current->getNext();
+    }
+}
